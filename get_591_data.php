be4e63b4a833a0a8f2f2ad73ef2a8e02b47bb766
wade0926 <posipsy@gmail.com>
2015-08-13 16:40:51 +0800

    v 1.0

--- get_591_data.php
+++ get_591_data.php
@@ -3,65 +3,75 @@ require_once("../db.php");
 set_time_limit(0);
 mysql_query("SET NAMES 'UTF8'");
 date_default_timezone_set('Asia/Taipei');
-?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>get 591</title>
-</head>
-<?php
-//Step 1： 抓591 id
-//get_591_id($para_totalRows = 15903);
+//設定要執行的步驟
+$act = 0;
 
-//Step 2： 抓個人詳細資料
-//設定撈取的限制次數
-$get_limit = 200;
-
-$sql = "SELECT 5_id,5_name_id,5_name FROM 591_link_data WHERE 5_name = '' AND 5_del = 0 LIMIT ".$get_limit;	
-$res = mysql_query($sql);
-	            
-while($row = mysql_fetch_array($res))
+if($act == 3)
 {
-	if($row['5_name'] == '')
-	{			
-		$target_url = 'http://www.591.com.tw/'.$row['5_name_id'];		
-		$page_data = exec_curl($target_url);
-		
-		//找姓名
-		preg_match_all("/<span class=\"name\">.*?<h3>(.*?)<\/h3>/s",$page_data,$name);
-		$name = $name[1][0];
-		
-		//找就職公司
-		preg_match_all("/<li>就職公司：<span>(.*?)<\/span> <span>(.*?)<\/span><\/li>/",$page_data,$company);
-		$company = $company[1][0].' '.$company[2][0];
-		
-		//找行動電話
-		preg_match_all("/<li>行動電話：<span id=\"phone-num\" class=\"org\">(.*?)<\/span><\/li>/",$page_data,$cell_phone);
-		$cell_phone = $cell_phone[1][0];
-				
-		//找服務區域
-		preg_match_all("/<li>服務區域：<span>(.*?)<\/span><\/li>/",$page_data,$serve_area);
-		$serve_area = $serve_area[1][0];
-		
-		//找E-mail
-		preg_match_all("/<li class=\"long\">E-mail：<span>(.*?)<\/span><\/li>/",$page_data,$mail);
-		$mail = $mail[1][0];
-				
-		$sql = 'UPDATE 591_link_data SET 5_name = "'.$name.'",5_company = "'.$company.'",5_cell_phone = "'.$cell_phone.'",5_serve_area = "'.$serve_area.'",5_mail = "'.$mail.'" WHERE 5_id = '.$row['5_id'];		
-		mysql_query($sql);		
-	}	
+	header("Content-disposition: filename=abc123.xls");
+	header("Content-type:application/vnd.ms-excel;charset=UTF-8");
+	echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+}
+else
+{
+	?>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>get 591</title>
+    </head>
+    <body>
+    <?php	
 }
 
-echo 'done';			
+switch($act)
+{
+	//Step 1： 抓591 id
+	case 1:		
+		get_591_id($para_totalRows = 15903);		
+	break;
+	
+	//Step 2： 抓個人詳細資料
+	case 2:
+		//設定撈取的限制次數
+		$get_limit = 200;
+		
+		//抓個人詳細資料
+		get_detail($get_limit);
+	break;
+	
+	case 3:		
+		//查詢要畫table 的資料
+	    $sql = "SELECT * FROM 591_link_data WHERE 5_name != '' AND 5_del = 0";	
+	    $res = mysql_query($sql);		
+		?>
+		<table>
+			<?php			
+            while($row = mysql_fetch_array($res))
+            {
+                ?>                
+                <tr>
+                	<td><?php echo $row['5_name'];?></td>
+                    <td><?php echo $row['5_company'];?></td>
+                    <td><?php echo $row['5_cell_phone'];?></td>
+                    <td><?php echo $row['5_serve_area'];?></td>
+                    <td><?php echo $row['5_mail'];?></td>
+                </tr>                
+                <?php
+            }
+            ?>
+		</table>
+        <?php
+		exit;
+	break;
+}	
 ?>
-<body>
 </body>
 </html>
-
 <?php
-//抓591 id
+//Step 1：抓591 id
 function get_591_id($para_totalRows)
 {
 	//總頁數
@@ -77,7 +87,7 @@ function get_591_id($para_totalRows)
 		$page_data = exec_curl($target_url);
 		
 		//找出該頁每個人的id
-		preg_match_all("/<li link=\"(.*?)\" class/",$page_data,$link_data);
+		preg_match_all("/<li link=\"(.*?)\" class/",$page_data,$link_data);		
 				
 		foreach($link_data[1] as $row)
 		{	
@@ -99,6 +109,48 @@ function get_591_id($para_totalRows)
 		}	
 	}
 }
+
+//Step 2：抓個人詳細資料
+function get_detail($get_limit)
+{
+	//查詢「姓名」為空的
+	$sql = "SELECT 5_id,5_name_id,5_name FROM 591_link_data WHERE 5_name = '' AND 5_del = 0 LIMIT ".$get_limit;	
+	$res = mysql_query($sql);
+					
+	while($row = mysql_fetch_array($res))
+	{
+		if($row['5_name'] == '')
+		{			
+			$target_url = 'http://www.591.com.tw/'.$row['5_name_id'];		
+			$page_data = exec_curl($target_url);
+									
+			//找姓名
+			preg_match("/<span class=\"name\">.*?<h3>(.*?)<\/h3>/s",$page_data,$name);			
+			$name = $name[1];			
+			
+			//找就職公司
+			preg_match("/<li>就職公司：<span>(.*?)<\/span> <span>(.*?)<\/span><\/li>/",$page_data,$company);			
+			$company = $company[1].' '.$company[2];
+			
+			//找行動電話
+			preg_match("/<li>行動電話：<span id=\"phone-num\" class=\"org\">(.*?)<\/span><\/li>/",$page_data,$cell_phone);
+			$cell_phone = $cell_phone[1];
+					
+			//找服務區域
+			preg_match("/<li>服務區域：<span>(.*?)<\/span><\/li>/",$page_data,$serve_area);
+			$serve_area = $serve_area[1];
+			
+			//找E-mail
+			preg_match("/<li class=\"long\">E-mail：<span>(.*?)<\/span><\/li>/",$page_data,$mail);			
+			$mail = $mail[1];
+								
+			$sql = 'UPDATE 591_link_data SET 5_name = "'.$name.'",5_company = "'.$company.'",5_cell_phone = "'.$cell_phone.'",5_serve_area = "'.$serve_area.'",5_mail = "'.$mail.'" WHERE 5_id = '.$row['5_id'];		
+			mysql_query($sql);		
+		}	
+	}
+	
+	echo 'done';	
+}	
 
 function exec_curl($target_url)
 {
